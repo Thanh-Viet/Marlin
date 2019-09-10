68cab949172cf93f9dda5e47fb90946cacae5903
Thanh-Viet <bertrand.jenner@gmail.com>
2019-09-10 08:21:12 +0000

    Just added user

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(thisiskeithb, Ender-3)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Thanh-Viet, Ender-3 Pro SKR1.3)" // Who made the changes.
 
 /**
  * *** VENDORS PLEASE READ ***
